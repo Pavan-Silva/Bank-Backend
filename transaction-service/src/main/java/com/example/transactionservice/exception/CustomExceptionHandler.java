8e71cda520dee495bf6843a6d512497f98763198
Pavan-Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-03-06 10:45:42 +0530

    build - switched to rest controller advice in transaction service

--- transaction-service/src/main/java/com/example/transactionservice/exception/CustomExceptionHandler.java
+++ transaction-service/src/main/java/com/example/transactionservice/exception/CustomExceptionHandler.java
@@ -3,37 +3,33 @@ package com.example.transactionservice.exception;
 import com.example.transactionservice.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler {
+
     @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception, HttpServletRequest req) {
-        return new ResponseEntity<>(
-                ErrorResponse.builder()
-                        .status(HttpStatus.NOT_FOUND.value())
-                        .timestamp(Instant.now())
-                        .error("Not found")
-                        .message(exception.getMessage())
-                        .path(req.getServletPath())
-                        .build()
-                , HttpStatus.NOT_FOUND);
+    public ErrorResponse handleNotFoundException(NotFoundException exception, HttpServletRequest req) {
+        return ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .timestamp(Instant.now())
+                .error("Not found")
+                .message(exception.getMessage())
+                .path(req.getServletPath())
+                .build();
     }
 
     @ExceptionHandler({BadRequestException.class})
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException exception, HttpServletRequest req) {
-        return new ResponseEntity<>(
-                ErrorResponse.builder()
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .timestamp(Instant.now())
-                        .error("Bad Request")
-                        .message(exception.getMessage())
-                        .path(req.getServletPath())
-                        .build()
-                , HttpStatus.BAD_REQUEST);
+    public ErrorResponse handleBadRequestException(BadRequestException exception, HttpServletRequest req) {
+        return ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(Instant.now())
+                .error("Bad Request")
+                .message(exception.getMessage())
+                .path(req.getServletPath())
+                .build();
     }
 }
