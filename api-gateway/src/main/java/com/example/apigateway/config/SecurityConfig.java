637856594a6eb7f6bbd66979697e1a527f9eff09
Pavan-Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-03-09 19:49:27 +0530

    fix - restricted email service only for admin users

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -2,10 +2,19 @@ package com.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,9 +28,30 @@ public class SecurityConfig {
                                 exchange.pathMatchers("/eureka/**").permitAll()
                                         .pathMatchers("/actuator/**").permitAll()
                                         .pathMatchers("/users/register").permitAll()
+                                        .pathMatchers("/email/**").hasRole("ADMIN")
                                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oath -> oath.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oath -> oath.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(converterForKeycloak()))
+                )
                 .build();
+    }
+
+    @Bean
+    ReactiveJwtAuthenticationConverter converterForKeycloak() {
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = realmAccess.get("roles");
+
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        };
+
+        var converter = new ReactiveJwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(jwtGrantedAuthoritiesConverter));
+
+        return converter;
     }
 }
